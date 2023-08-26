aea6bec4ebd9889dedccef7d6670ded16b322bf2
Stephen Zhang <stephen.zsy@gmail.com>
2023-08-26 14:15:18 -0500

    debug

--- frontend/app/debug/page.tsx
+++ frontend/app/debug/page.tsx
@@ -2,6 +2,6 @@ import _ from "lodash";
 import { headers } from "next/headers";
 
 export default function DebugPage() {
-  const allHeaders = _.toArray(headers().entries);
+  const allHeaders = _.toArray(headers().entries());
   return <pre>{JSON.stringify(allHeaders, null, 2)}</pre>;
 }
