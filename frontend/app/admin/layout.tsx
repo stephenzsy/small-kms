31231fb67d18cc21c8d3add447b8936d91e153f9
Stephen Zhang <stephen.zsy@gmail.com>
2023-08-26 14:07:55 -0500

    frontend

--- frontend/app/admin/layout.tsx
+++ frontend/app/admin/layout.tsx
@@ -5,10 +5,11 @@ import type { PropsWithChildren } from "react";
 export default function AdminLayout(props: PropsWithChildren<{}>) {
   const auth = getMsAuth();
   if (!auth.isAdmin) {
-    const e = new NextResponse(undefined, {
-      status: 403,
-    });
-    return e;
+    return (
+      <div className="max-w-7xl p-6 mx-auto">
+        You must have administrator access
+      </div>
+    );
   }
   return <div className="max-w-7xl p-6 mx-auto">{props.children}</div>;
 }
