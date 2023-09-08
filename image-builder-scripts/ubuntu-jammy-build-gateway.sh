9e09cd25ad8192f3dcbbab0e64941194d12f7967
Stephen Zhang <stephen.zsy@gmail.com>
2023-09-08 00:55:37 -0500

    update script to include azure-cli

--- image-builder-scripts/ubuntu-jammy-build-gateway.sh
+++ image-builder-scripts/ubuntu-jammy-build-gateway.sh
@@ -1,22 +1,40 @@
 #!/bin/bash -e
 
 apt-get -q update
-apt-get -q install ca-certificates curl gnupg
-
+apt-get -q -y install ca-certificates curl gnupg \
+  apt-transport-https lsb-release
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
     apt-get -q remove $pkg;
 done
+
+apt-get -q -y upgrade
 
 # Add Docker's official GPG key:
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
+# Add Microsoft GPG key:
+curl -sLS https://packages.microsoft.com/keys/microsoft.asc |  gpg --dearmor | tee /etc/apt/keyrings/microsoft.gpg > /dev/null
+chmod go+r /etc/apt/keyrings/microsoft.gpg
 
 # Add the repository to Apt sources:
+## Docker
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
+## Azure CLI
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=`dpkg --print-architecture` signed-by=/etc/apt/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+  sudo tee /etc/apt/sources.list.d/azure-cli.list  
+
 apt-get -q update
 
+# Install Docker Engine
 apt-get -q -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# Install Azure CLI
+apt-get -q -y install azure-cli
+
+# Configure vm as a router
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
