4f8ca2b4c936e6d6266ab0f05246902ffe9bfc7d
Stephen Zhang <stephen.zsy@gmail.com>
2023-11-15 11:40:31 -0600

    missing files for FE

---
+++ frontend/src/agents/page.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "antd";
+
+export default function AgentsPage() {
+  return (
+    <>
+      <Typography.Title>Agents</Typography.Title>
+    </>
+  );
+}
---
+++ frontend/src/system/page.tsx
@@ -0,0 +1,53 @@
+import { Button, Card, Typography } from "antd";
+import { useSystemAppRequest } from "../admin/forms/useSystemAppRequest";
+import { Link } from "../components/Link";
+
+function SystemAppsEntry({ systemAppName }: { systemAppName: string }) {
+  const { data: systemApp, run: syncSystemApp } =
+    useSystemAppRequest(systemAppName);
+  return (
+    <div className="space-y-4">
+      <dl>
+        <div>
+          <dt>Application ID (Client ID)</dt>
+          <dd className="font-mono">{systemApp?.id}</dd>
+        </div>
+        <div>
+          <dt>Display Name</dt>
+          <dd>{systemApp?.displayName}</dd>
+        </div>
+        <div>
+          <dt>Service Principal ID</dt>
+          <dd className="font-mono">{systemApp?.servicePrincipalId}</dd>
+        </div>
+      </dl>
+      <div className="flex items-center gap-4">
+        <Link to={`/system/${systemAppName}`}>View</Link>
+        <Button
+          type="link"
+          onClick={() => {
+            syncSystemApp(true);
+          }}
+        >
+          Sync
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default function SystemAppsPage() {
+  return (
+    <>
+      <Typography.Title>System applications</Typography.Title>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <Card title="API">
+          <SystemAppsEntry systemAppName="api" />
+        </Card>
+        <Card title="Backend">
+          <SystemAppsEntry systemAppName="backend" />
+        </Card>
+      </div>
+    </>
+  );
+}
